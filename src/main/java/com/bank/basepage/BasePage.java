edc1fffa7810e3a68520f57e4f9df6cc6cca7cfc
chaitanyamehta1 <mehtachaitanya1179@gmail.com>
2020-04-22 20:59:39 +0100

    xyz-bank-page-factory-master

---
+++ src/main/java/com/bank/basepage/BasePage.java
@@ -0,0 +1,18 @@
+package com.bank.basepage;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Chaitanya
+ */
+public class BasePage {
+    public static WebDriver driver;
+
+    public BasePage(){
+        PageFactory.initElements(driver,this);
+        PropertyConfigurator.configure(System.getProperty("user.dir")+"/src/test/java/com/bank/resources/propertiesfile/log4j.properties");
+    }
+
+}
